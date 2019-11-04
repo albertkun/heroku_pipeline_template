e110f1d544ad4f3fc5a9a79f2277d114e6bfed88
Albert Kochaphum <albertkun@idre.ucla.edu>
2019-11-04 11:35:12 -0800

    added app and proc file for heroku

---
+++ app.py
@@ -0,0 +1,17 @@
+### app.py
+### basic flask application to run a heroku pipeline
+
+# bring in flask
+from flask import Flask
+
+# define the application
+app = Flask(__name__)
+
+# set the default route
+@app.route('/')
+def index():
+    return 'hello heroku pipeline template'
+
+# set the script to run
+if __name__ == '__main__':
+    app.run(debug=True)
